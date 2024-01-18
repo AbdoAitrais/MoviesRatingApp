a8ebd1603850ee92c6fddfb1746c6ce59bbd7963
abdo <abdo-pro@hotmail.com>
2024-01-18 20:10:33 +0100

    replaced deprecated AsyncTask class with ExecutorService and added refresh button

--- app/src/main/java/com/example/javamoviesratingapp/MainActivity.java
+++ app/src/main/java/com/example/javamoviesratingapp/MainActivity.java
@@ -1,42 +1,45 @@
 package com.example.javamoviesratingapp;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import com.example.javamoviesratingapp.adapter.Adaptery;
 import com.example.javamoviesratingapp.model.Movie;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.IOException;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Link for popular movies https://api.themoviedb.org/3/movie/top_rated?api_key=96e65c274f8988d468a2f9b8e76f54a3
     private static final String JSON_URL = "https://api.themoviedb.org/3/movie/top_rated?api_key=96e65c274f8988d468a2f9b8e76f54a3";
-    private AppBarConfiguration appBarConfiguration;
 
     private List<Movie> movieList;
     private RecyclerView recyclerView;
-    private ImageView header;
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,81 +48,77 @@ public class MainActivity extends AppCompatActivity {
 
         movieList = new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView);
-        header = findViewById(R.id.imageView);
+        ImageView header = findViewById(R.id.imageView);
 
         Glide.with(this)
                 .load(R.drawable.header)
                 .into(header);
 
-        GetData getData = new GetData();
-        getData.execute();
+        Button refreshButton = findViewById(R.id.refreshButton);
+        refreshButton.setOnClickListener(v -> {
+            // Clear the movie list
+            movieList.clear();
+
+            // Execute the background task using ExecutorService
+            executorService.execute(() -> {
+                String result = fetchDataFromUrl();
+
+                // Post the results back to the UI thread
+                handler.post(() -> handleResult(result));
+            });
+        });
+
         // how to check the movie list in logcat
         System.out.println("Movie List: " + movieList);
     }
 
-    class GetData extends AsyncTask<String, String, String>{
+    private String fetchDataFromUrl() {
+        StringBuilder current = new StringBuilder();
+        try {
+            URL apiUrl = new URL(MainActivity.JSON_URL);
+            HttpURLConnection urlConnection = (HttpURLConnection) apiUrl.openConnection();
 
-        @Override
-        protected String doInBackground(String... strings) {
-            StringBuilder current = new StringBuilder();
             try {
-                URL url;
-                HttpURLConnection urlConnection = null;
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 
-                try {
-                    url = new URL(JSON_URL);
-                    urlConnection = (HttpURLConnection) url.openConnection();
-
-                    InputStream inputStream = urlConnection.getInputStream();
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-                    int data = inputStreamReader.read();
-                    while (data != -1){
-                        current.append((char) data);
-                        data = inputStreamReader.read();
-                    }
-
-                    return current.toString();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e){
-                    e.printStackTrace();
-                } finally {
-                    if (urlConnection != null){
-                        urlConnection.disconnect();
-                    }
+                int data = inputStreamReader.read();
+                while (data != -1) {
+                    current.append((char) data);
+                    data = inputStreamReader.read();
                 }
-            } catch (Exception e){
-                e.printStackTrace();
+
+                return current.toString();
+            } finally {
+                urlConnection.disconnect();
             }
-            return current.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return current.toString();
+    }
+
+    private void handleResult(String result) {
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+
+                Movie model = new Movie();
+                model.setId(jsonObject1.getString("vote_average"));
+                model.setName(jsonObject1.getString("title"));
+                model.setImage(jsonObject1.getString("poster_path"));
+
+                movieList.add(model);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Error occurred during JSON parsing");
         }
 
-        @Override
-        protected void onPostExecute(String s) {
-            try {
-                JSONObject jsonObject = new JSONObject(s);
-                JSONArray jsonArray = jsonObject.getJSONArray("results");
-
-                for (int i = 0; i < jsonArray.length(); i++){
-                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
-
-                    Movie model = new Movie();
-                    model.setId(jsonObject1.getString("vote_average"));
-                    model.setName(jsonObject1.getString("title"));
-                    model.setImage(jsonObject1.getString("poster_path"));
-                    // show the movie list in logcat
-                    System.out.println("Movie: " + model.getName());
-                    movieList.add(model);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-                // log the error
-                Log.e("MainActivity", "Error occurred during JSON parsing");
-            }
-
-            putDataIntoRecyclerView(movieList);
-        }
+        putDataIntoRecyclerView(movieList);
     }
 
     private void putDataIntoRecyclerView(List<Movie> movieList) {
